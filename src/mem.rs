86e34bdb763950da3f231c7215a07ac5597e01a9
Newton Ni <nwtnni@gmail.com>
2019-05-08 10:13:07 -0400

    Start implementing memory

--- src/mem.rs
+++ src/mem.rs
@@ -7,3 +7,26 @@ impl From<u16> for Addr {
         Addr(addr & 0x0FFF)
     }
 }
+
+#[derive(Clone)]
+pub struct Mem(Box<[u8; 4096]>);
+
+impl Mem {
+    pub fn new() -> Self {
+        let mut mem = Box::new([0; 4096]);
+        Mem(mem)
+    }
+}
+
+impl std::ops::Index<Addr> for Mem {
+    type Output = u8;
+    fn index(&self, addr: Addr) -> &Self::Output {
+        &self.0[addr.0 as usize]
+    }
+}
+
+impl std::ops::IndexMut<Addr> for Mem {
+    fn index_mut(&mut self, addr: Addr) -> &mut Self::Output {
+        &mut self.0[addr.0 as usize]
+    }
+}
