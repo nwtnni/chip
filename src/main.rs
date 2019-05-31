2cc9eb818b678b15c417b7f4fe703b6dea211f49
Newton Ni <nwtnni@gmail.com>
2019-05-31 18:59:13 -0400

    Integrate structopt for nicer CLI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::io::Write;
 use std::time;
+use std::path;
+
+use structopt::StructOpt;
 
 use termion::cursor;
 use termion::clear;
@@ -7,14 +10,28 @@ use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-const TICK: time::Duration = time::Duration::from_nanos(16_666_666);
+/// 1s / 60Hz delay between sound and delay timer decrements.
+const DELAY: time::Duration = time::Duration::from_nanos(16_666_666);
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "chip", about = "A terminal-based CHIP-8 emulator.")]
+struct Opt {
+    /// CPU frequency in hertz.
+    #[structopt(long = "hz", default_value = "1000")] 
+    hz: u32,
+
+    /// Binary CHIP-8 ROM file to emulate.
+    #[structopt(parse(from_os_str))]
+    path: path::PathBuf,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = std::env::args().collect::<Vec<_>>();
-    let path = &args[1];
-    let file = std::fs::read(path)?;
+    let args = Opt::from_args();
+    let file = std::fs::read(args.path)?;
+
     let mut chip = chip::Chip::new(file);
-    let mut timer = std::time::Instant::now();
+    let mut timer = time::Instant::now();
+    let pause = time::Duration::from_secs(1) / args.hz;
 
     let stdin = termion::async_stdin();
     let mut stdout = std::io::stdout().into_raw_mode()?;
@@ -30,7 +47,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         | _ => chip.clear_key(),
         }
 
-        if std::time::Instant::now() - timer > TICK {
+        if std::time::Instant::now() - timer > DELAY {
             timer = std::time::Instant::now();
             chip.tick();
         }
@@ -38,6 +55,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         chip.step();
         chip.draw(&mut stdout)?;
         stdout.flush()?;
+        std::thread::sleep(pause);
     }
 
     write!(stdout, "{}{}{}", clear::All, cursor::Goto(0, 0), cursor::Show)?;
